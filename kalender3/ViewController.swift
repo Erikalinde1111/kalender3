194e850c7dc83e94f935a14e440bd455bc5d06bb
Erika Linde <erika.j.linde@gmail.com>
2017-03-08 20:29:42 +0100

    Initial Commit

---
+++ kalender3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kalender3
+//
+//  Created by Erika Linde on 08/03/17.
+//  Copyright © 2017 Erika Linde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
